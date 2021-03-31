0e74532fae9ae6381d2a14f888f911bafdabf3e4
RSijelmass <R.Sijelmass@gmail.com>
2021-03-31 18:59:36 +0100

    Add CashHolder to VendingMachine
    when someone calls #select_product, a call is made to an instance of 
    CashHolder to store the funds.

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,14 +1,16 @@
 class VendingMachine
-  def initialize(products)
+  def initialize(products, cash_holder = CashHolder.new)
     @products = products
+    @cash_holder = cash_holder
   end
 
   def select_product(product_name, money_input)
     product = @products.detect { |product| product.name == product_name }
-    
+
     raise "This item does not exist." unless product
     raise "Not enough money provided." unless money_input >= product.price
 
+    @cash_holder.store_funds(money_input)
     product
   end
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -5,17 +5,37 @@ RSpec.describe VendingMachine do
   let(:mars) { double("mars", name: "mars", price: 65) }
   let(:twix) { double("twix", name: "twix", price: 75) }
   let(:products) { [snickers, mars, twix] }
-  let(:vending_machine) { VendingMachine.new(products) }
+  let(:cash_holder) { instance_double("cash_holder", current_value: 0) }
+  let(:vending_machine) { VendingMachine.new(products, cash_holder) }
 
   describe 'select product' do
-    it 'returns a product if I give enough money' do
+    before do
+      allow(cash_holder).to receive(:store_funds)
+    end
+
+    context 'with enough funds' do
+      it 'returns a product' do
         expect(vending_machine.select_product("snickers", 100)).to eq snickers
+      end
+      it 'calls to CashHolder to #store_funds' do
+        funds_to_store = 100
+        expect(cash_holder).to receive(:store_funds).with(funds_to_store)
+        vending_machine.select_product("snickers", funds_to_store)
+      end
     end
-    it 'throws an exception if I do not give enough money' do
-      expect{ vending_machine.select_product("snickers", 5) }.to raise_exception("Not enough money provided.")
+
+    context 'with non-acceptable funds' do
+      it 'throws an exception if I do not give enough money' do
+        expect{ vending_machine.select_product("snickers", 5) }
+          .to raise_exception("Not enough money provided.")
+      end
     end
-    it 'throws an exception if I select a product that does not exist' do
-      expect{ vending_machine.select_product("some-other-snack", 100) }.to raise_exception("This item does not exist.")
+
+    context 'with a non-existent product' do
+      it 'throws an exception' do
+        expect{ vending_machine.select_product("some-other-snack", 100) }
+          .to raise_exception("This item does not exist.")
+      end
     end
   end
 end
