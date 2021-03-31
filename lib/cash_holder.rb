df933c5bea17a4ea8bcc88cde59ee9b32bc4f58d
RSijelmass <R.Sijelmass@gmail.com>
2021-03-31 18:26:53 +0100

    Adds method #store_funds to CashHolder
    Adds given funds to current_value in CashHolder, if the funds provided 
    are not negative. This is to ensure no one can take money out of the 
    machine through this method.

--- lib/cash_holder.rb
+++ lib/cash_holder.rb
@@ -1,7 +1,12 @@
 class CashHolder
   attr_reader :current_value
-  
+
   def initialize(current_value=0)
     @current_value = current_value
+  end
+
+  def store_funds(given_funds)
+    raise "No negative funds allowed." if given_funds.negative?
+    @current_value += given_funds
   end
 end
--- spec/cash_holder_spec.rb
+++ spec/cash_holder_spec.rb
@@ -1,12 +1,13 @@
 require 'cash_holder'
 
 RSpec.describe CashHolder do
-
+  let(:input_value) { 500 }
+  let(:cash_holder) { CashHolder.new(input_value) }
   describe 'initialise cash holder' do
     it 'can be intialised with an input value' do
-      input_value = 500
-      cash_holder = CashHolder.new(input_value)
-      expect(cash_holder.current_value).to eq input_value
+      new_input_value = 100
+      cash_holder = CashHolder.new(new_input_value)
+      expect(cash_holder.current_value).to eq new_input_value
     end
     it 'can be initialised with no input value' do
       cash_holder = CashHolder.new
@@ -15,11 +16,12 @@ RSpec.describe CashHolder do
   end
 
   describe '#store_funds' do
-    xit 'updates the current_value with the input given' do
-
+    it 'updates the current_value with the input given' do
+      added_value = 50
+      expect { cash_holder.store_funds(added_value) }.to change { cash_holder.current_value }.by(added_value)
     end
-    xit 'does not allow negative funds (taking funds out)' do
-
+    it 'does not allow negative funds (taking funds out)' do
+      expect { cash_holder.store_funds(-50) }.to raise_error "No negative funds allowed."
     end
   end
 end
